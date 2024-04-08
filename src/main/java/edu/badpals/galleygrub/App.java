6fb2aa88eb445cbadd21ff302cc19649fe294fe5
AmarinoLama <amarinolama@gmail.com>
2024-04-08 10:26:16 +0200

    feat (App) : add class main

---
+++ src/main/java/edu/badpals/galleygrub/App.java
@@ -0,0 +1,40 @@
+package edu.badpals.galleygrub;
+
+/**
+ *
+ * Bob Esponja trabaja como cocinero en el Krusty Krab,
+ * restaurante famoso por su hamburguesa de autor
+ * Krabby Patty.
+ * El restaurante tiene un menu llamado Galley Grub
+ * que puedes leer aquí:
+ * https://spongebob.fandom.com/wiki/Galley_Grub
+ *
+ * Las comandas son anotadas a mano por Squidward Tentacles.
+ * El propierario Eugene H. Krabs -un agarrao-
+ * en vez de adquirir un TPV ha decidido contratar
+ * a un estudiante de Dual para que programar un software
+ * para calcular el importe total del pedido e imprimir el recibo.
+ *
+ * Intenta hacer como que programas si no quieres terminar
+ * en la cocina ayudando a Bob a preparar Krusty Burguers.
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        /**
+         * Crea un diccionario con los precios
+         * de los extras.
+         *
+         *  Los precios de los extras son estos:
+         *  - cheese +0.25
+         *  - sauce +0.50
+         *  - medium +0.25
+         *  - large +0.50
+         *
+         * Lee los precios de los items en la imagen
+         * del menu proporcionada.
+         */
+
+    }
