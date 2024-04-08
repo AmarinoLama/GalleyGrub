57e807c3673f9596bbf13e790b72da10e6fe2ef5
AmarinoLama <amarinolama@gmail.com>
2024-04-08 10:42:32 +0200

    feat (Item) : add methods

--- src/main/java/edu/badpals/galleygrub/items/Item.java
+++ src/main/java/edu/badpals/galleygrub/items/Item.java
@@ -1,4 +1,48 @@
 package edu.badpals.galleygrub.items;
 
-public class Item {
+public class Item implements Product{
+
+    String name;
+    Double price;
+    String extra;
+
+    public Item(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Item(String name, Double price, String extra) {
+        this.name = name;
+        this.price = price;
+        this.extra = extra;
+    }
+
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public Double price() {
+        return price;
+    }
+
+    @Override
+    public String extra() {
+        return extra;
+    }
+
+    @Override
+    public Boolean isRegular() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", extra='" + extra + '\'' +
+                '}';
+    }
 }
