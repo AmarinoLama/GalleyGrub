677c118fe46c7a9a14b23d9cb5c1494aeacc3412
AmarinoLama <amarinolama@gmail.com>
2024-04-11 12:49:25 +0200

    feat (Receipt) : add methods

--- src/main/java/edu/badpals/galleygrub/receipt/Receipt.java
+++ src/main/java/edu/badpals/galleygrub/receipt/Receipt.java
@@ -1,4 +1,46 @@
 package edu.badpals.galleygrub.receipt;
 
-public class Receipt {
+import edu.badpals.galleygrub.extras.Extra;
+import edu.badpals.galleygrub.items.Item;
+import edu.badpals.galleygrub.order.Comanda;
+
+public class Receipt implements Ticket {
+
+    private Double total;
+    private Comanda order;
+
+    public Receipt(Comanda comanda) {
+        this.order = comanda;
+    }
+
+    @Override
+    public Comanda getOrder() {
+        return order;
+    }
+
+    @Override
+    public void setChain(Extra extra) {
+
+    }
+
+    @Override
+    public Double total() {
+        double counter = 0d;
+        for (Item item: order.itemList()) {
+            counter += item.price();
+        }
+        this.total = counter;
+        return counter;
+    }
+
+    @Override
+    public void sumExtrasCharge() {
+
+    }
+
+    @Override
+    public void print() {
+        order.display();
+        System.out.println("\tTOTAL --------> " + total() + "$");
+    }
 }
