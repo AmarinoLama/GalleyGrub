f3606f5ae7362161884738a816193c02d8884f24
AmarinoLama <amarinolama@gmail.com>
2024-04-11 12:35:59 +0200

    feat (Ticket) : finish interface

--- src/main/java/edu/badpals/galleygrub/receipt/Ticket.java
+++ src/main/java/edu/badpals/galleygrub/receipt/Ticket.java
@@ -1,4 +1,12 @@
 package edu.badpals.galleygrub.receipt;
 
+import edu.badpals.galleygrub.extras.Extra;
+import edu.badpals.galleygrub.order.Comanda;
+
 public interface Ticket {
+    Comanda getOrder();
+    void setChain(Extra extra);
+    Double total();
+    void sumExtrasCharge();
+    void print();
 }
