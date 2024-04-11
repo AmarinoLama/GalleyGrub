303943d488d864c5d7a0515aee856b8791bbfb69
AmarinoLama <amarinolama@gmail.com>
2024-04-11 12:15:25 +0200

    feat (Order) : implement methods

--- src/main/java/edu/badpals/galleygrub/order/Order.java
+++ src/main/java/edu/badpals/galleygrub/order/Order.java
@@ -1,4 +1,42 @@
 package edu.badpals.galleygrub.order;
 
-public class Order {
+import edu.badpals.galleygrub.items.Item;
+
+import java.util.List;
+
+public class Order implements Comanda {
+    @Override
+    public void addItem(String name, double price) {
+
+    }
+
+    @Override
+    public void addItem(String name, double price, String extra) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public List<Item> itemList() {
+        return null;
+    }
+
+    @Override
+    public Double getTotal() {
+        return null;
+    }
+
+    @Override
+    public void updateTotal(Double total) {
+
+    }
+
+    @Override
+    public void display() {
+
+    }
 }
