34ce017dc0e52d5d3e6fe079ae66a7ecf1cb9e99
AmarinoLama <amarinolama@gmail.com>
2024-04-15 22:22:15 +0200

    test (ItemTest) : add tests

--- src/test/java/edu/badpals/galleygrub/items/ItemTest.java
+++ src/test/java/edu/badpals/galleygrub/items/ItemTest.java
@@ -1,4 +1,17 @@
 package edu.badpals.galleygrub.items;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class ItemTest {
+    @Test
+    public void equals_test() {
+
+        Item patty = new Item("Krabby Patty", 1.25);
+        Item kpatty = new Item("Krabby Patty", 1.25);
+
+        assertTrue(!patty.equals(kpatty));
+
+    }
 }
