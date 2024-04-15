4f65e8ad6e97b451bcb673e2010a2335aeb76094
AmarinoLama <amarinolama@gmail.com>
2024-04-15 08:49:01 +0200

    feat (Extra) : finish abstract class

--- src/main/java/edu/badpals/galleygrub/extras/Extra.java
+++ src/main/java/edu/badpals/galleygrub/extras/Extra.java
@@ -1,4 +1,23 @@
 package edu.badpals.galleygrub.extras;
 
+import edu.badpals.galleygrub.order.Comanda;
+
+import java.util.Optional;
+
 public abstract class Extra {
+    final static String CHEESE = "";
+    final static String SAUCE = "";
+    final static String SIZE_LARGE = "";
+
+    String extraProduct;
+    Optional<Extra> nextExtra;
+
+    public Extra() {
+    }
+
+    public void setNextExtra(Extra nextExtra) {
+        this.nextExtra = Optional.ofNullable(nextExtra);
+    }
+
+    public abstract void sumExtras(Comanda comanda);
 }
