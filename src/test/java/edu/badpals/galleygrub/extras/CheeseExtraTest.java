89be5ba5e21cc442898e7e12990e4ebe951ed359
AmarinoLama <amarinolama@gmail.com>
2024-04-15 10:30:54 +0200

    test (CheeseExtraTest) : add class

---
+++ src/test/java/edu/badpals/galleygrub/extras/CheeseExtraTest.java
@@ -0,0 +1,12 @@
+package edu.badpals.galleygrub.extras;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import edu.badpals.galleygrub.order.Order;
+import edu.badpals.galleygrub.receipt.Receipt;
+
+public class CheeseExtraTest {
+
+}
